240bec7e6db8bedbb2c3f551536ee07700c713af
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-06 09:10:40 +0100

    Base de datos creada

--- E3.sql
+++ E3.sql
@@ -1,26 +1,26 @@
+DROP DATABASE E3;
 CREATE DATABASE E3 COLLATE = utf8_general_ci;
-
 USE E3;
 
 CREATE TABLE zinema (
-    id_zinema smallint,
+    id_zinema smallint auto_increment,
     email varchar(50),
     helbidea varchar(50),
     zinema_izena varchar(20),
-    areto_zbk int,
+    areto_zbk smallint,
     ordutegia varchar(20),
     telefonoa int,
     primary key (id_zinema)
 );
 
 CREATE TABLE aretoa (
-    id_aretoa smallint,
+    id_aretoa smallint auto_increment,
     id_zinema smallint,
     primary key (id_aretoa)
 );
 
 CREATE TABLE filma (
-    id_filma smallint,
+    id_filma smallint auto_increment,
     film_izena varchar(50),
     generoa enum('Drama', 'Komedia', 'Zi-Fi', 'Beldurra') not null,
     prezioa double,
@@ -29,21 +29,22 @@ CREATE TABLE filma (
 );
 
 CREATE TABLE saioa (
-    id_saioa smallint,
+    id_saioa smallint auto_increment,
     saioaren_eguna date,
     ordutegia varchar(20),
     id_aretoa smallint,
     id_filma smallint,
+    id_zinema smallint,
     primary key (id_saioa)
 );
 
 CREATE TABLE erosketa (
-    id_erosketa smallint,
+    id_erosketa smallint auto_increment,
     deskontua varchar(10),
     erosketa_eguna date,
     kantitatea smallint,
     factura double,
-    id_bezeroa varchar(9),
+    NAN varchar(9),
     primary key (id_erosketa)
 );
 
@@ -56,34 +57,34 @@ CREATE TABLE bezeroa (
 );
 
 CREATE TABLE sarrera (
-    id_sarrera smallint,
+    id_sarrera smallint auto_increment,
     id_erosketa smallint,
     id_saioa smallint,
-    primary key (id_sarrera)
+    primary key (id_sarrera, id_erosketa)
 );
 
--- ALTER TABLE
+-- ALTER TABLE (Foreign Key)
 
-ALTER TABLE aretoa
-ADD CONSTRAINT fk_zinema
-FOREIGN KEY (id_zinema) REFERENCES zinema(id_zinema);
+ALTER TABLE ARETOA
+ADD 
+FOREIGN KEY (id_zinema) REFERENCES ZINEMA(Id_zinema);
 
-ALTER TABLE saioa
-ADD CONSTRAINT fk_aretoa
-FOREIGN KEY (id_aretoa) REFERENCES aretoa(id_aretoa);
+ALTER TABLE SAIOA
+ADD 
+FOREIGN KEY (id_filma) REFERENCES FILMA(Id_filma),
+ADD 
+FOREIGN KEY (id_aretoa) REFERENCES ARETOA(id_aretoa),
+ADD 
+FOREIGN KEY (id_zinema) REFERENCES ZINEMA(Id_zinema);
 
-ALTER TABLE saioa
-ADD CONSTRAINT fk_filma
-FOREIGN KEY (id_filma) REFERENCES filma(id_filma);
+ALTER TABLE SARRERA
+ADD 
+FOREIGN KEY (id_erosketa) REFERENCES EROSKETA(id_erosketa),
+ADD 
+FOREIGN KEY (id_saioa) REFERENCES SAIOA(id_saioa);
 
-ALTER TABLE sarrera
-ADD CONSTRAINT fk_erosketa
-FOREIGN KEY (id_erosketa) REFERENCES erosketa(id_erosketa);
+ALTER TABLE EROSKETA
+ADD 
+FOREIGN KEY (NAN) REFERENCES BEZEROA(NAN);
 
-ALTER TABLE sarrera
-ADD CONSTRAINT fk_saioa
-FOREIGN KEY (id_saioa) REFERENCES saioa(id_saioa);
-
-ALTER TABLE erosketa
-ADD CONSTRAINT fk_bezeroa
-FOREIGN KEY (id_bezeroa) REFERENCES bezeroa(NAN);
+-- ALTER TABLE (Konfigurazioak)
